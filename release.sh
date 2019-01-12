88c05d5001e0a461659acd323c969827b2924978
Storm Heg <storm@stormheg.co>
2019-01-12 22:12:13 +0100

    version 1.0.2

--- release.sh
+++ release.sh
@@ -20,7 +20,7 @@ echo "Version: $version"
 # tag it
 git add -A
 git commit -m "version $version"
-git tag -a "$version" -m "version $version"
+git tag -s -a "$version" -m "version $version"
 git push
 git push --tags
 
